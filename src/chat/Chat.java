e66c799a66e8e8a8d784124aa9c71f2d80f46a35
azimshaik <cheer.buddu@gmail.com>
2013-08-12 18:00:07 -0400

    Completed chat client

--- src/chat/Chat.java
+++ src/chat/Chat.java
@@ -6,7 +6,10 @@ import javax.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
-public class Chat {
+public class Chat implements MessageListener {
+	private TopicSession pubSession;
+	private TopicConnection connection;
+	private TopicPublisher publisher;
 	public Chat(String topicName) throws Exception {
 		Properties env = new Properties();
 		env.put(Context.SECURITY_PRINCIPAL, "guest");
@@ -17,7 +20,39 @@ public class Chat {
 		InitialContext jndi = new InitialContext(env);
 		TopicConnectionFactory conFactory = (TopicConnectionFactory) jndi.lookup("JmsTopicConnectionFactory");
 		TopicConnection connection = conFactory.createTopicConnection();
+		TopicSession pubSession = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+		TopicSession subSession = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+		Topic chatTopic = (Topic)jndi.lookup(topicName);
+		TopicPublisher publisher = pubSession.createPublisher(chatTopic);
+		TopicSubscriber subscriber = subSession.createSubscriber(chatTopic);
+		subscriber.setMessageListener(this);
+		this.pubSession = pubSession;
+		this.connection = connection;
+		this.publisher = publisher;
 		connection.start();
+	}
+	public void writeMessage(String text)
+	{
+		TextMessage textMessage;
+		try {
+			textMessage = pubSession.createTextMessage();
+			textMessage.setText(text);
+			publisher.publish(textMessage);
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+	public void onMessage(Message message) {
+		// TODO Auto-generated method stub
+		TextMessage textmessage = (TextMessage) message;
+		try {
+			String text = textmessage.getText();
+			System.out.println(text);
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
--- src/chat/RunChat.java
+++ src/chat/RunChat.java
@@ -1,9 +1,17 @@
 package chat;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class RunChat {
 	public static void main(String[] args){
 		try {
 			Chat chat = new Chat("test");
+			BufferedReader commandLine = new BufferedReader(new InputStreamReader(System.in));
+			while(true){
+				String s = commandLine.readLine();
+				chat.writeMessage(s);
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
