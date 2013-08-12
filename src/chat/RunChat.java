f31c0afe1d3d05d07cf12494f8c69dbf44564e4e
azimshaik <cheer.buddu@gmail.com>
2013-08-12 17:17:26 -0400

    Added run chat

--- src/chat/RunChat.java
+++ src/chat/RunChat.java
@@ -1,5 +1,13 @@
 package chat;
 
 public class RunChat {
+	public static void main(String[] args){
+		try {
+			Chat chat = new Chat("test");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
